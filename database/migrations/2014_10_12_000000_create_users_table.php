b86ef0505e18e0d378d623ed8563fe62dac10659
Andini Andin <aandini072@gmail.com>
2021-06-09 17:21:05 +0700

    Mengedit schema table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 15)->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role', ['adm', 'usr'])->nullable()->default('adm');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
