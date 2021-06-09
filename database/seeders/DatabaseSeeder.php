b19badbe5df915e8b815ca62e07ca480e7f84ffc
Andini Andin <aandini072@gmail.com>
2021-06-10 02:05:52 +0700

    Menambahkan seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Administrator',
+            'address' => 'Malang',
+            'phone' => '08512345678',
+            'email' => 'admin@gmail.com',
+            'role' => "adm",
+            'password' => Hash::make('admin1234')
+        ]);
     }
 }
