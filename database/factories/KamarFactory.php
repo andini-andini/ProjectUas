faefc5fb4f9cb792998af53d71878d9796835205
Andini Andin <aandini072@gmail.com>
2021-06-09 17:22:35 +0700

    Membuat file factory kamar

---
+++ database/factories/KamarFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Kamar;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class KamarFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Kamar::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
