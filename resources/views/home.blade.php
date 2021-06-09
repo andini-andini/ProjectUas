532d2f682e291ffeaf68c53e9b29f32dc96ad3ca
Andini Andin <aandini072@gmail.com>
2021-06-09 18:42:26 +0700

    Mengubah tampilan dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+<div class="page-breadcrumb bg-white">
+    <div class="row align-items-center">
+        <div class="col-12">
+            <div class="d-md-flex">
+                <ol class="breadcrumb ms-auto">
+                    <li><a class="fw-normal">Dashboard</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <h3 class="box-title">Halo, {{Auth::user()->name}}. Selamat Datang Di Sistem Informasi Hotel</h3>
             </div>
         </div>
     </div>
