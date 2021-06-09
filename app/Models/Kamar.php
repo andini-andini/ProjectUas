122930cf6468bff7828405eb69be373454fb66c3
Andini Andin <aandini072@gmail.com>
2021-06-09 17:32:33 +0700

    Edit model kamar

--- app/Models/Kamar.php
+++ app/Models/Kamar.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Kamar extends Model
 {
     use HasFactory;
+
+    protected $table = 'kamar';
+
+    protected $fillable = [
+        'name',
+        'price',
+        'image',
+        'description',
+    ];
+
+    public function fasilitas()
+    {
+        return $this->belongsToMany(Fasilitas::class, 'kamar_fasilitas', 'kamar_id', 'fasilitas_id');
+    }
 }
