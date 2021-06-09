fd4d7e11164134ab5f8e14bd6d1fd364fcb2d54b
Andini Andin <aandini072@gmail.com>
2021-06-09 19:19:41 +0700

    Menambahkan route baru

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FasilitasController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KamarController;
 use App\Http\Controllers\ReservasiController;
 use App\Http\Controllers\UserController;
@@ -18,14 +20,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('beranda');
+Route::get('/data-kamar', [HomeController::class, 'kamar'])->name('beranda.kamar');
+Route::get('/data-fasilitas', [HomeController::class, 'fasilitas'])->name('beranda.fasilitas');
+Route::get('/data-kamar/{id}', [HomeController::class, 'showKamar'])->name('beranda.showkamar');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('user', UserController::class);
-Route::resource('fasilitas', FasilitasController::class);
-Route::resource('kamar', KamarController::class);
-Route::resource('reservasi', ReservasiController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('dashboard', DashboardController::class);
+    Route::middleware(['admin'])->group(function () {
+        Route::resource('user', UserController::class);
+        Route::resource('fasilitas', FasilitasController::class);
+        Route::resource('kamar', KamarController::class);
+        Route::resource('reservasi', ReservasiController::class);
+    });
+});
