4aff1799dcbc72582ede9cf9d8a892306ee2661c
Amelia Widya <aandini072@gmail.com>
2021-06-11 13:53:29 +0700

    Mengubah default admin menjadi user pada migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('phone', 15)->nullable();
             $table->text('address')->nullable();
-            $table->enum('role', ['adm', 'usr'])->nullable()->default('adm');
+            $table->enum('role', ['adm', 'usr'])->nullable()->default('usr');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
